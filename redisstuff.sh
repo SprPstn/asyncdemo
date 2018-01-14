6628514eb4fecc9aef4f662bca7ed8e7ec84e6a9
Superposition <ericmanganaro@gmail.com>
2018-01-14 19:48:42 +0000

    redis files for installation'

---
+++ redisstuff.sh
@@ -0,0 +1,12 @@
+
+#this is for downloading latest version
+wget http://download.redis.io/redis-stable.tar.gz
+
+# Extracting
+tar xvzf redis-stable.tar.gz
+
+# Changing to folder
+cd redis-stable
+
+# Installing
+make
